a7c3a22368eb7d740a64d5b9a7c6bdd775c98554
pandaJian <pandaJian@example.com>
2021-03-02 17:32:39 +0800

    frist test

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+   <h1> 116寝室 M78星云</h1>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
   </div>
 </template>

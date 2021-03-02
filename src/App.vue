426b24bedada644ac547cd4bf962ddffaf49f22b
pandajian <1014668770@qq.com>
2021-03-02 18:52:47 +0800

    2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
-   <h1> 116寝室 M78星云</h1>
+   <h1>M78星云</h1>
    <img src="http://img12.360buyimg.com/n1/s450x450_jfs/t1/142295/6/19866/63874/5fe4408eE401889c8/8f5c2b37676a2264.jpg"/>
+   <img src="https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2413398312,997663359&fm=26&gp=0.jpg"/>
+   <img src="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3168704571,2572178064&fm=26&gp=0.jpg"/>
+   <img src="https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1057266467,784420394&fm=26&gp=0.jpg"/>
   </div>
 </template>
 
